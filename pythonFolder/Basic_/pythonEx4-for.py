454b4679b633b465d70446820a92a68ad9b4da4c
maizurzu <101496219+maizurzu@users.noreply.github.com>
2022-08-19 01:51:25 +0900

    Delete pythonFolder/Basic_ directory

--- pythonFolder/Basic_/pythonEx4-for.py
+++
@@ -1,9 +0,0 @@
-family = ['mother', 'father', 'son']
-
-for x in family:
-    print(x)
-
-# range
-
-for x in range(4, 12):
-    print(x)
--- pythonFolder/Basic_/패키지.py
+++
@@ -1,9 +0,0 @@
-import 딕셔너리 as dic
-
-#패키지 내용 살펴보기
-#dir(dic);
-
-print(dic.x['a'])
-
-#선택적 임포트
-from 객체지향 import Rectangle, worrior1
